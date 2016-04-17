cf0aa53b4a8e6c0d1cdea7cd24ff128b9c5b2084
Sangil-Lee <silee7103@gmail.com>
2016-04-17 15:48:32 +0900

    DSP

---
+++ MATLAB/DSP/05.Assignment/dtft_test.m
@@ -0,0 +1,10 @@
+%w = -2*pi:0.01:2*pi;
+w = -pi:0.01:pi;
+n = 0:1:100;
+w0 = 2;
+x = exp(n .* (1i*w0)); % x = exp(jw0n)
+
+X = dtft(x, n, w);
+
+subplot(2,1,1); plot(n, x); title('signal = exp(jw0n)');
+subplot(2,1,2); plot(w, X); title('DTFT');
