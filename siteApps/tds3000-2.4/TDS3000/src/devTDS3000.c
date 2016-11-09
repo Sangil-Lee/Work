e5308883ba84e557149a733f777b6171280af8e9
Sangil-Lee <silee7103@gmail.com>
2016-11-09 10:54:16 +0900

    Tektronix TDS3000 GPIB Interface Module

---
+++ siteApps/tds3000-2.4/TDS3000/src/devTDS3000.c
@@ -0,0 +1,16 @@
+#define FAMILY       3000
+#define EFASTI_TERM
+#define FAMILY_VDIV_TABLE "1.0E-3",  \
+                          "2.0E-3",  \
+                          "5.0E-3",  \
+                          "1.0E-2",  \
+                          "2.0E-2",  \
+                          "5.0E-2",  \
+                          "1.0E-1",  \
+                          "2.0E-1",  \
+                          "5.0E-1",  \
+                          "1.0E0",   \
+                          "2.0E0",   \
+                          "5.0E0",   \
+                          "1.0E1", 
+#include "devTDS.c"
