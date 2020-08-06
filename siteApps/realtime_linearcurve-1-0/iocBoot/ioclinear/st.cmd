840eeca2616762e547f18e37a1fae2f5d4961322
Sangil Lee <silee7103@gmail.com>
2020-08-06 15:52:48 +0900

    linear/polynomial regression example with epics

---
+++ siteApps/realtime_linearcurve-1-0/iocBoot/ioclinear/st.cmd
@@ -0,0 +1,21 @@
+#!../../bin/linux-x86_64/linear
+
+#- You may have to change linear to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/linear.dbd"
+linear_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=ctrluser")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=ctrluser"
