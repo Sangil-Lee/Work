b45541e2f56394b5edf55ea6e9d703b660109119
silee7103 <silee7103@gmail.com>
2020-09-07 08:53:22 -0400

    SCL3 Cryo-Module ModbusTCP Address Mapping

--- siteApps/SCL3-LinacCM-1-0/iocBoot/iocscl3linac/st.cmd
+++ siteApps/SCL3-LinacCM-1-0/iocBoot/iocscl3linac/st.cmd
@@ -12,11 +12,31 @@ cd "${TOP}"
 dbLoadDatabase "dbd/scl3linac.dbd"
 scl3linac_registerRecordDeviceDriver pdbbase
 
+#Siemens 1200/1500 PLC -> Default Port:502
+#drvAsynIPPortConfigure("SCL3", "192.168.0.3:502", 0, 0, 1)
+#modbusInterposeConfig("SCL3",0,5000,0)
+
+##Siemens ModbusTCP Address Map according to modbus function code.
+
+#Read Holding Register Address(0-699), Modbus F.C: 5)
+###Start Address(0Bit, 173(32bit, 4bytes, 2words))############################
+#drvModbusAsynConfigure("SCL3DB_AIn", "SCL3", 0, 3,  0,  175,  0, 100, "Modicon")
+
+#Write Single Holding Register Address(700Bit, 175(32bit, 4bytes, 2words)), Modbus F.C: 15,16)
+###Start Address(700Bit, 175(32bit, 4bytes, 2words))############################
+#drvModbusAsynConfigure("SCL3DB_AOut","SCL3", 0, 16,  175, 325,  0, 100, "Modicon")
+
+#drvModbusAsynConfigure("SCL3DB_DIn","SCL3", 0, 3,  500, 14,  0, 100, "Modicon")
+
+#Write Discreate Input(DI, 1-24, Address(0~23), Modbus F.C: 2)
+#drvModbusAsynConfigure("SCL3DB_DOut","SCL3", 0, 6,  514, 11,  0, 100, "Modicon")
+
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=ctrluser")
 dbLoadTemplate("${TOP}/iocBoot/${IOC}/scl3_macro_test.sub", "SYS=${SYS},SUBSYS=${SUBSYS},DEV=${DEV1},SUBDEV=${SUBDEV1}")
 
 dbLoadTemplate("${TOP}/iocBoot/${IOC}/scl3_macro_test.sub", "SYS=${SYS},SUBSYS=${SUBSYS},DEV=${DEV2},SUBDEV=${SUBDEV2}")
+
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
