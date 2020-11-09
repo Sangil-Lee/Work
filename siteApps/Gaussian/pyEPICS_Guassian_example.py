3f71ef07c019ec04a87c7767690f4b0009cf7c67
silee7103 <silee7103@gmail.com>
2020-11-09 07:52:54 -0500

    Test Gaussian Example

--- siteApps/Gaussian/pyEPICS_Guassian_example.py
+++ siteApps/Gaussian/pyEPICS_Guassian_example.py
@@ -26,18 +26,30 @@ mypv.add_callback(onChanges)
 
 t0=time.time()
 
+
 """while time.time() - t0 < 5.0:"""
 while True:
    bvalue = bval
    if bvalue:
        x = np.linspace(-10, 10, 100)
-       y = func(x, 2, 0, 2)
+       """y = func(x, 2, 0, 2)"""
+       y = func(x, 1e-5, 0, 3 )
        np.random.seed(int(time.time()))
        yn = y+0.2*np.random.normal(size=len(x))
        popt, pcov = curve_fit(func, x, yn)
        print(popt)
        wavepv = PV('LEBT-DIAG:WS002:WValue')
-       wavepv.put(x, wait=True)
+       wavepv.put(func(x, *popt), wait=True)
+       pvgroup = (PV('LEBT-DIAG:WS002:Amp'), PV('LEBT-DIAG:WS002:Cent'),PV('LEBT-DIAG:WS002:Sigma') )
+       newvals = (popt[0], popt[1], popt[2])
+       for pv, val in zip(pvgroup, newvals):
+           pv.put(val, use_complete=True)
+           
+       waiting = True
+       while waiting:
+           time.sleep(0.001)
+           waiting = not all([pv.put_complete for pv in pvgroup])
+       print('All put are done')
    time.sleep(1)
    
 print('Done')
