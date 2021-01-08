f39026d5014d255022b47d46a7c14629b073cab0
Sangil Lee <silee7103@gmail.com>
2021-01-08 17:02:54 +0900

    Example open valve orbit according to difference of pressure

---
+++ siteApps/SCL3Cooldown-sim-1-0/iocBoot/ioccooldown/st.cmd
@@ -0,0 +1,23 @@
+#!../../bin/linux-x86_64/cooldown
+
+#- You may have to change cooldown to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/cooldown.dbd"
+cooldown_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=ctrluser")
+dbLoadRecords("db/check_condition_press.vdb")
+#dbLoadTemplate("${TOP}/iocBoot/${IOC}/scl3_seq_simulate.sub")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=ctrluser"
