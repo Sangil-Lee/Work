bf0008a7e9afec852de66d831dced6126d823014
Sangil Lee <silee7103@gmail.com>
2021-02-25 14:33:06 +0900

    Added CDFanoutLogic Sequence

--- siteApps/SCL3Cooldown-sim-2-O/iocBoot/ioccooldown/st.cmd
+++ siteApps/SCL3Cooldown-sim-2-O/iocBoot/ioccooldown/st.cmd
@@ -16,17 +16,21 @@ cooldown_registerRecordDeviceDriver pdbbase
 #dbLoadRecords("db/check_condition_press.vdb")
 #dbLoadRecords("db/check_condition_heline_circulation.vdb", "SYS=SCL31, SUBSYS=CDL01, DEV=VBx02")
 
-dbLoadTemplate("${TOP}/iocBoot/${IOC}/scl3_seq_simulate.sub")
-dbLoadRecords("db/testwaveform.vdb")
+#dbLoadTemplate("${TOP}/iocBoot/${IOC}/scl3_seq_simulate.sub")
+#dbLoadRecords("db/testwaveform.vdb") #for. seq sncMakeTempWaveform
 
 #dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic.sub", "SYS=SCL31, SUBSYS=-CDL01:, DEV=VBx02, SUBDEV=-CV7301:")
-#dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_QWR.sub", "SYS=SCL31, SUBSYS=-CDL01:")
-#dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_HWRA.sub", "SYS=SCL31, SUBSYS=-CDL01:")
-#dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_HWRB.sub", "SYS=SCL31, SUBSYS=-CDL01:")
-#dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_P2DT.sub", "SYS=SCL31, SUBSYS=-CDL01:")
+
+dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_QWR.sub", "SYS=SCL31, SUBSYS=-CDL01:")
+dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_HWRA.sub", "SYS=SCL31, SUBSYS=-CDL01:")
+dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_HWRB.sub", "SYS=SCL31, SUBSYS=-CDL01:")
+dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_P2DT.sub", "SYS=SCL31, SUBSYS=-CDL01:")
+dbLoadRecords("db/TestCountPV.vdb")
+
 #dbLoadRecords("db/CDFanoutTest.vdb","SYS=SCL3, SUBSYS=-CoolDown:")
 #dbLoadRecords("db/fanout_db_field.vdb","SYS=SCL3, SUBSYS=-CoolDown:")
 #dbLoadRecords("db/TempEval.vdb")
+
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
@@ -34,4 +38,5 @@ iocInit
 ## Start any sequence programs
 #seq CDOperationSeq
 #seq CDFanoutTest
-seq sncMakeTempWaveform
+#seq sncMakeTempWaveform
+seq CDFanoutLogic
