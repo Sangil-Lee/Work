71552df3acc319e4028164af12c13d7a98df9589
Sangil Lee <silee7103@gmail.com>
2021-03-10 13:49:17 +0900

    Linear(Polynomial) Regression with Eigen into Process Variable

---
+++ siteApps/linearRegression-1-0/iocBoot/ioclregression/st.cmd
@@ -0,0 +1,21 @@
+#!../../bin/linux-x86_64/lregression
+
+#- You may have to change lregression to something else
+#- everywhere it appears in this file
+
+< envPaths
+
+cd "${TOP}"
+
+## Register all support components
+dbLoadDatabase "dbd/lregression.dbd"
+lregression_registerRecordDeviceDriver pdbbase
+
+## Load record instances
+#dbLoadRecords("db/xxx.db","user=ctrluser")
+
+cd "${TOP}/iocBoot/${IOC}"
+iocInit
+
+## Start any sequence programs
+#seq sncxxx,"user=ctrluser"
