e5e594e9a0eaf6be64cbc001fa87b8cc212fe41a
silee7103 <silee7103@gmail.com>
2021-06-10 21:00:31 +0900

    Dynamic PV assign & monitoring at Sequencer

--- siteApps/ValveEvalTest-1-0/iocBoot/iocvalveEval/st.cmd
+++ siteApps/ValveEvalTest-1-0/iocBoot/iocvalveEval/st.cmd
@@ -13,10 +13,13 @@ valveEval_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=ctrluser")
+dbLoadRecords("db/OM031AdditionalPV.vdb")
 dbLoadTemplate("${TOP}/iocBoot/${IOC}/cdom141_6.sub")
+dbLoadTemplate("${TOP}/iocBoot/${IOC}/cooldownlogic_eval_OM0.3_step1.sub", "SYS=SCL31, SUBSYS=-CDL01:")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 ## Start any sequence programs
 #seq sncxxx,"user=ctrluser"
+seq CDOM031Logic
