7d89f24a696a54bc50297fa6c1be6dd09bb3ad56
Sangil Lee <silee7103@gmail.com>
2021-07-05 11:54:00 +0900

    ...

--- siteApps/SCL3Cooldown-phase-end-1-0/iocBoot/iocphasend/st.cmd
+++ siteApps/SCL3Cooldown-phase-end-1-0/iocBoot/iocphasend/st.cmd
@@ -18,13 +18,18 @@ dbLoadRecords("db/aiOutLinkTest.vdb")
 #dbLoadTemplate("db/sncTempWF.sub","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:")
 
 dbLoadRecords("db/sncTempEvalWF.vdb")
-dbLoadTemplate("db/sncTempEvalWF.sub","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:")
+dbLoadTemplate("db/sncTempEvalWF.sub","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=-Temp:")
+
+dbLoadRecords("db/sncPressEvalWF.vdb")
+dbLoadTemplate("db/sncPressEvalWF.sub","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=-Press:")
+
 #dbLoadRecords("db/phaseEndCond_TempMon.vdb","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:, COUNT=5")
-#dbLoadRecords("db/PhaseEnd.vdb","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:, COUNT=5")
-dbLoadRecords("db/PhaseEnd.vdb","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:, COUNT=20")
+dbLoadRecords("db/PhaseEnd.vdb","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:, COUNT=5")
+#dbLoadRecords("db/PhaseEnd.vdb","SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:, COUNT=20")
 
 dbLoadTemplate("db/scl3_seq_simulate_temp.sub")
-#dbLoadTemplate("db/scl3_seq_simulate_press.sub")
+dbLoadTemplate("db/scl3_seq_simulate_press.sub")
+
 #dbLoadTemplate("db/scl3_seq_simulate.sub")
 
 #dbLoadTemplate("${TOP}/iocBoot/${IOC}/scl3_seq_simulate.sub")
@@ -34,6 +39,7 @@ iocInit
 ## Start any sequence programs
 #seq sncxxx,"user=ctrluser"
 #seq sncTempWF,"SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:"
-seq sncTempEvalWF,"SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:"
+seq sncTempEvalWF,"SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=-Temp:"
+seq sncPressEvalWF,"SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=-Press:"
 seq sncPhaseEnd,"SYS=SCL3,SUBSYS=:, DEV=Cooldown, SUBDEV=:"
 
--- siteApps/SCL3Cooldown-phase-end-1-0/phasendApp/src/GenPhaseEnd.py
+++ siteApps/SCL3Cooldown-phase-end-1-0/phasendApp/src/GenPhaseEnd.py
@@ -1,4 +1,5 @@
-#!/usr/local/bin/python3.8
+########/usr/local/bin/python3.8
+#!/usr/bin/python3
 
 import sys
 
