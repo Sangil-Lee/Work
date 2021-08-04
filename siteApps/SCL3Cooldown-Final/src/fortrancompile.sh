96d3a8a6a20c86fd5ad294d12251127d3a35f9c3
Sangil Lee <silee7103@gmail.com>
2021-08-04 17:17:50 +0900

    ...

---
+++ siteApps/SCL3Cooldown-Final/src/fortrancompile.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+MACHINE_TYPE=`uname -m`
+DIRNAME=''
+if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	DIRNAME='O.linux-x86_64'
+elif [ ${MACHINE_TYPE} == 'x86' ]; then
+	DIRNAME='O.linux-x86'
+elif [ ${MACHINE_TYPE} == 'armv7l' ]; then
+	DIRNAME='O.linux-arm'
+fi
+
+if [ ! -d ${DIRNAME} ]; then
+	mkdir ${DIRNAME}/
+fi
+
+g++ -c -fPIC Dnst_Viscosity.f -lgfortran
+mv Dnst_Viscosity.o ${DIRNAME}/
