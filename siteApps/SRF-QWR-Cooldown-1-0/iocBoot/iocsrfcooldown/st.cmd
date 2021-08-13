7039c04df2a3332d97d27c60074cfc0b8e6cb2ba
Sangil Lee <silee7103@gmail.com>
2021-08-13 17:45:22 +0900

    ...

--- siteApps/SRF-QWR-Cooldown-1-0/iocBoot/iocsrfcooldown/st.cmd
+++ siteApps/SRF-QWR-Cooldown-1-0/iocBoot/iocsrfcooldown/st.cmd
@@ -14,12 +14,13 @@ srfcooldown_registerRecordDeviceDriver pdbbase
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=ctrluser")
 dbLoadTemplate("db/Additional.sub")
-dbLoadRecords("db/RefValue.vdb",  "SYS=SRF01,SUBSYS=-Bunker1:,DEV=QWR01,SUBDEV=-Temp:")
-dbLoadTemplate("db/cooldownlogic_eval_OM1.4_step6_SRF.sub", "SYS=SRF01, SUBSYS=-Bunker1:, DEV=QWR01, SUBDEV=-Valve:, SIGNAL=Valve")
+dbLoadRecords("db/RefValue.vdb",  "SYS=SRF01,SUBSYS=-Bunker1:,DEV=HWRB01,SUBDEV=-Temp:")
+#dbLoadTemplate("db/cooldownlogic_eval_OM1.4_step6_SRF.sub", "SYS=SRF01, SUBSYS=-Bunker1:, DEV=QWR01, SUBDEV=-Valve:, SIGNAL=Valve")
+dbLoadTemplate("db/SRF_HWR_Cooldown.sub", "SYS=SRF01, SUBSYS=-Bunker1:, DEV=HWRB01, SUBDEV=-Valve:, SIGNAL=Valve")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 ## Start any sequence programs
 #seq sncxxx,"user=ctrluser"
-
+#seq sncSRFCooldown, "SYS=SRF01,SUBSYS=-Bunker1:, DEV=HWR01, SUBDEV=-Logic:"
