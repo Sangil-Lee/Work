917cc9e1d5629939680fc637060305b873be6a0d
Sangil Lee <silee7103@gmail.com>
2021-12-23 17:27:51 +0900

    Add Template

--- siteApps/SCL3_MAG-1-0/iocBoot/iocscl3magnet/st.cmd
+++ siteApps/SCL3_MAG-1-0/iocBoot/iocscl3magnet/st.cmd
@@ -15,17 +15,16 @@ scl3magnet_registerRecordDeviceDriver pdbbase
 
 
 ######SCL3 Quadrupole Magnet Power Supply######
-#drvAsynIPPortConfigure ("SCL3_QM1", "192.168.84.6:4001", 0, 0, 0)
+#drvAsynIPPortConfigure ("SCL3_QM1",    "192.168.84.6:4001", 0, 0, 0)
+#drvAsynIPPortConfigure ("SCL3_QM1_ST", "192.168.84.6:4002", 0, 0, 0)
 
 ## According to SCL3 Quadrupole Maganet Type
 ## Load record instances
-#dbLoadTemplate("${TOP}/iocBoot/${IOC}/danfysik_SW.sub", "DEVICE=KOBRA-MAG:Q9-PS:,   port=SCL3_QM1")
+#dbLoadTemplate("${TOP}/iocBoot/${IOC}/danfysik_SW.sub", "SYS=SCL31, SUBSYS=-QWR01:, DEV=QMAD01, SUBDEV=-PSU01:, port=SCL3_QM1")
+#dbLoadTemplate("${TOP}/iocBoot/${IOC}/danfysik_SW.sub")
 
-## According to KoBRA Maganet Type
-#dbLoadRecords("db/dbAutoRampup.db", "DEVICE=KOBRA-MAG:Q9-PS")
-
-## According to KoBRA Maganet Type
-#dbLoadRecords("db/MGField2Cur_Q9.vdb",   "DEVICE=KOBRA-MAG:Q9-PS")
+##Toyotech P.S
+#dbLoadRecords("db/toyotech.db", "SYS=MEBT,SUBSYS=QM5,DEV=ToyoTech, port=SCL3_QM1_ST")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
