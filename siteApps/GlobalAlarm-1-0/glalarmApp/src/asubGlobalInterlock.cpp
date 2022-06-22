dfeffe3471138b27db3a6725e2ee5fc921fd3f77
Sangil-Lee <silee7103@gmail.com>
2022-06-23 00:22:30 +0900

    Global Interlock Logic

--- siteApps/GlobalAlarm-1-0/glalarmApp/src/asubGlobalInterlock.cpp
+++ siteApps/GlobalAlarm-1-0/glalarmApp/src/asubGlobalInterlock.cpp
@@ -4,7 +4,9 @@
 #include <fstream>
 #include <iomanip>
 #include <bitset>
+#include <vector>
 #include <algorithm>
+#include <unordered_map>
 #include <cmath>
 #include <complex>
 #include <iostream>
@@ -27,12 +29,18 @@
 
 #define	OUTFILE 0
 
+#define hash_map std::unordered_map
+
 using namespace std;
 static int gbintDebug = 0;
+
+hash_map<int, int> hashDelay;
 
 //
 static long InitGBInterlock(aSubRecord *pRec)
 {
+	for(int i = 0;i < 11; i++)
+		hashDelay[i] = 0;
     return(0);
 }
 
@@ -61,11 +69,42 @@ static long ProcGBInterlock(aSubRecord *pRec)
 	/*INPF
 	 * Cryo-CDL00:TBx01-XV7381:CloseStat
 	 */
-	
+
 
 	//OUTA
 
-	long nelm = pRec->noa;
+	int nelm = (int)pRec->noa;
+	double *inpval = (double*)pRec->a;
+	unsigned char *outval = (unsigned char*)pRec->vala;
+
+	float *inpd = (float*)pRec->d;
+
+	char *b = (char*)pRec->b;
+	char *c = (char*)pRec->c;
+	double *e = (double*)pRec->e;
+	char *f = (char*)pRec->f;
+
+	//int bc = (int)((int*)pRec->b[0]) && (int)(((int*)pRec->c[0]);
+
+	for(int i = 0; i < nelm; i++)
+	{
+		double inpa = (double)inpval[i];
+		bool test = (inpa < inpd[0] && inpa < e[0] )? false : b[0]&&c[0]&&f[0] ? false : true;
+
+		if(test)
+			hashDelay[i] += 1;
+		else
+			hashDelay[i] = 0;
+
+		if(hashDelay[i] > 3)
+			outval[i] = 1;
+		else
+			outval[i] = 0;
+	}
+
+
+
+
 	double *sinval = (double*)pRec->a;
 
 #if 1
