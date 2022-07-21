bae87961ac1bf93147da3bd3e2434a1d49d5ff72
Sangil Lee <silee7103@gmail.com>
2022-07-21 09:00:06 +0900

    rm no neccessary

--- siteApps/WinShmIOC/ioctestApp/src/O.windows-x64-mingw/initTrace.d
+++
@@ -1,7 +0,0 @@
-initTrace.obj: ../initTrace.c C:/epics/7.0.6.1/base/include/initHooks.h \
- C:/epics/7.0.6.1/base/include/libComAPI.h \
- C:/epics/7.0.6.1/base/include/epicsExport.h \
- C:/epics/7.0.6.1/base/include/shareLib.h \
- C:/epics/7.0.6.1/base/include/iocsh.h \
- C:/epics/7.0.6.1/base/include/compilerDependencies.h \
- C:/epics/7.0.6.1/base/include/compiler/gcc/compilerSpecific.h
--- siteApps/WinShmIOC/ioctestApp/src/O.windows-x64-mingw/xxxRecord.h.d
+++
@@ -1,6 +0,0 @@
-../O.Common/xxxRecord.h: ../xxxRecord.dbd \
-    C:/epics/7.0.6.1/base/dbd/dbCommon.dbd
-
-../xxxRecord.dbd:
-C:/epics/7.0.6.1/base/dbd/dbCommon.dbd:
-"../O.Common/xxxRecord.h: ../Makefile" 
