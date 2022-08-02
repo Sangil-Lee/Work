e17d06cb9f0f88849017ec815f05f208374dd1f3
Sangil-Lee <silee7103@gmail.com>
2022-08-02 14:20:41 +0900

    tester

--- siteApps/cdmsTester-1-0/iocBoot/ioccdmstester/st.cmd
+++ siteApps/cdmsTester-1-0/iocBoot/ioccdmstester/st.cmd
@@ -11,11 +11,13 @@ cd "${TOP}"
 dbLoadDatabase "dbd/cdmstester.dbd"
 cdmstester_registerRecordDeviceDriver pdbbase
 
-drvSyncRTPConfigure("RTPDevice", "89.89.80.30:50199 TCP", 0, 0)
+#drvSyncRTPConfigure("RTPDevice", "89.89.80.30:50199 TCP", 0, 0)
 
 
 #dbLoadRecords("db/RTPOneTest.vdb")
-dbLoadRecords("db/RTPTest.vdb")
+#dbLoadRecords("db/RTPTest.vdb")
+
+dbLoadRecords("db/CDMSTester.db", "PREFIX=CDMSTester")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
