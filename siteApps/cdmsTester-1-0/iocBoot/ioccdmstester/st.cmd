5c95b19d5055834a388bb4bb5e6bd61d78701d63
Sangil-Lee <silee7103@gmail.com>
2022-08-12 17:37:48 -0700

    add nidaq lib

--- siteApps/cdmsTester-1-0/iocBoot/ioccdmstester/st.cmd
+++ siteApps/cdmsTester-1-0/iocBoot/ioccdmstester/st.cmd
@@ -14,26 +14,25 @@ cdmstester_registerRecordDeviceDriver pdbbase
 #### Tested RTP-Protocol########
 #drvSyncRTPConfigure("RTPDevice", "192.168.1.50:50199 TCP", 0, 0)
 #dbLoadRecords("db/RTPOneTest.vdb")
-
-
 #dbLoadRecords("db/RTPTest.vdb")
 
-dbLoadRecords("db/CDMSTester.db", "PREFIX=CDMSTester")
 
 ##ModbusTCP
-drvAsynIPPortConfigure("CDMS_RTP", "192.168.1.61:502", 0, 0, 1)
+#drvAsynIPPortConfigure("CDMS_RTP", "192.168.1.61:502", 0, 0, 1)
 #drvAsynIPPortConfigure("CDMS_RTP", "192.168.1.50:50199", 0, 0, 1)
-modbusInterposeConfig("CDMS_RTP",0,5000,0)
+#modbusInterposeConfig("CDMS_RTP",0,5000,0)
 
 ## to 0   up 250 bytes(125 word), data block
-drvModbusAsynConfigure("CDMSAI",   "CDMS_RTP", 0, 3,  0,   100, 7, 100, "Modicon")  
+#drvModbusAsynConfigure("CDMSAI",   "CDMS_RTP", 0, 3,  0,   100, 7, 100, "Modicon")  
 #drvModbusAsynConfigure("CDMSAI",   "CDMS_RTP", 0, 3,  40000,   100, 7, 100, "Modicon")  
-
 #drvModbusAsynConfigure("CDMSAO",   "CDMS_RTP", 0, 6,  400, 125, 7, 100, "Modicon")
 #drvModbusAsynConfigure("CDMSDI", "CDMS_RTP", 0, 3,  550,  26, 7, 100, "Modicon")
 #drvModbusAsynConfigure("CDMSDO", "CDMS_RTP", 0, 6,  576,  22, 7, 100, "Modicon")
+#dbLoadTemplate("db/cdmsTester_Modbus.sub");
 
-dbLoadTemplate("db/cdmsTester_Modbus.sub");
+
+##Soft Pvs
+dbLoadRecords("db/CDMSTester.db", "PREFIX=CDMSTester")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
