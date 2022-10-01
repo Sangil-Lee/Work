1af23f83847107bbf9b04c91688dd8b2ea041412
Sangil-Lee <silee7103@gmail.com>
2022-10-01 14:52:43 +0900

    ...

---
+++ siteApps/winpxie-1-0/dllPath.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+SET "PATH=c:/home/cdms/epics/7.0.7/siteApps/Ctrl_IOC/winpxie-1-0/bin/windows-x64;C:/home/cdms/epics/7.0.7/base/bin/windows-x64;%PATH%"
+SET "PATH=C:/home/cdms/epics/7.0.7/epicsLibs/synApps/support/asyn-R4-36/bin/windows-x64;%PATH%"
+SET "PATH=C:/home/cdms/epics/7.0.7/epicsLibs/synApps/support/modbus-R3-0/bin/windows-x64;%PATH%"
+SET "PATH=C:/home/cdms/epics/7.0.7/epicsLibs/synApps/support/calc-R3-7-3/bin/windows-x64;%PATH%"
+SET "PATH=C:/home/cdms/epics/7.0.7/epicsLibs/synApps/support/sscan-R2-11-3/bin/windows-x64;%PATH%"
+SET "PATH=C:/home/cdms/epics/7.0.7/epicsLibs/synApps/support/std-R3-6/bin/windows-x64;%PATH%"
+SET "PATH=C:/home/cdms/epics/7.0.7/epicsLibs/synApps/support/seq-2-2-6/bin/windows-x64;%PATH%"
+
---
+++ siteApps/winpxie-1-0/vc64.bat
@@ -0,0 +1,2 @@
+set EPICS_HOST_ARCH=windows-x64
+"c:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
