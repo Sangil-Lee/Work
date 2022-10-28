43bd17601c197c1622fb593502f06e263021fdba
Sangil-Lee <silee7103@gmail.com>
2022-10-28 20:10:21 +0900

    ...

--- siteApps/NDPS-1-0/iocBoot/iocndps/st.cmd
+++ siteApps/NDPS-1-0/iocBoot/iocndps/st.cmd
@@ -12,40 +12,70 @@ dbLoadDatabase "dbd/ndps.dbd"
 ndps_registerRecordDeviceDriver pdbbase
 
 ###Phytron phymotion controller
-drvAsynIPPortConfigure("NDPSPhytron", "192.168.1.100:22222", 0, 0, 1)
+drvAsynIPPortConfigure("NDPSPhytron", "192.168.1.101:22222", 0, 0, 1)
 
 ###phytronCreateController (phytronPort, asynPort, movingPollPeriod, idlePollPeriod, timeout)
 phytronCreateController("PhyMotion_1", "NDPSPhytron", 100, 100, 1000)
 
 ###phytronCreateAxis(phytronPort, module, axis)
 phytronCreateAxis("PhyMotion_1", 1, 1)
-phytronCreateAxis("PhyMotion_1", 2, 1)
+#phytronCreateAxis("PhyMotion_1", 2, 1)
 
+###################################
+#Phytron Motor DB
+dbLoadTemplate("db/phytronMotor.sub")
 
-###ModbusTCP to Siemens PLC
-#drvAsynIPPortConfigure("NDPSSiemensPLC", "192.168.1.210:502", 0, 0, 1)
-#modbusInterposeConfig("NDPSSiemensPLC",0,5000,0)
-##drvModbusAsynConfigure("NDPSPLC_AI",  "NDPSSiemensPLC", 0, 3,  ??,   2, 7, 100, "Modicon")  
-##drvModbusAsynConfigure("NDPSPLC_AI",  "NDPSSiemensPLC", 0, 6,  ??,   2, 7, 100, "Modicon")  
+###################################
 
+###################################
+#Siemens PLC 
+#drvAsynIPPortConfigure("NDPS_WSPLC", "192.168.0.1:502", 0, 0, 1)
+#modbusInterposeConfig("NDPS_WSPLC", 0,5000,0)
+
+## Wire Scanner PLC(3)
+#drvModbusAsynConfigure("NDPSWSAI", "NDPS_WSPLC", 0, 3,  0,   8,  7, 100, "Modicon")  
+#drvModbusAsynConfigure("NDPSWSAO", "NDPS_WSPLC", 0, 6,  40,  20, 7, 100, "Modicon")
+#drvModbusAsynConfigure("NDPSWSDI", "NDPS_WSPLC", 0, 3,  110, 2,  7, 100, "Modicon")
+#drvModbusAsynConfigure("NDPSWSDO", "NDPS_WSPLC", 0, 6,  112, 2,  7, 100, "Modicon")
+#dbLoadTemplate("db/NDPS_WS.sub")
+
+#Siemens CCS PLC 
+drvAsynIPPortConfigure("NDPS_CCSPLC", "192.168.1.11:502", 0, 0, 1)
+modbusInterposeConfig("NDPS_CCSPLC",   0,5000,0)
+drvModbusAsynConfigure("NDPSCCSAI", "NDPS_CCSPLC", 0, 3,  0,   118,  7, 100, "Modicon")  
+drvModbusAsynConfigure("NDPSCCSAO", "NDPS_CCSPLC", 0, 6,  120, 120, 7, 100, "Modicon")
+drvModbusAsynConfigure("NDPSCCSDI", "NDPS_CCSPLC", 0, 3,  240, 5,  7, 100, "Modicon")
+drvModbusAsynConfigure("NDPSCCSDO", "NDPS_CCSPLC", 0, 6,  246, 10,  7, 100, "Modicon")
+dbLoadTemplate("db/NDPS_CCS.sub")
+
+#Siemens Vacuum PLC 
+drvAsynIPPortConfigure("NDPS_VACPLC", "192.168.1.1:502", 0, 0, 1)
+modbusInterposeConfig("NDPS_VACPLC",   0,5000,0)
+drvModbusAsynConfigure("NDPSVACAI", "NDPS_VACPLC", 0, 3,  0,   98,  7, 100, "Modicon")  
+drvModbusAsynConfigure("NDPSVACAO", "NDPS_VACPLC", 0, 6,  100, 98, 7, 100, "Modicon")
+drvModbusAsynConfigure("NDPSVACDI", "NDPS_VACPLC", 0, 3,  200, 15,  7, 100, "Modicon")
+drvModbusAsynConfigure("NDPSVACDO", "NDPS_VACPLC", 0, 6,  216, 15,  7, 100, "Modicon")
+dbLoadTemplate("db/NDPS_VAC.sub")
+
+###################################
 
 
 ###ModbusTCP to FlowMeter
-drvAsynIPPortConfigure("NDPSFlowMeter", "192.168.1.250:502", 0, 0, 1)
-modbusInterposeConfig("NDPSFlowMeter",0,5000,0)
+##drvAsynIPPortConfigure("NDPSFlowMeter", "192.168.1.250:502", 0, 0, 1)
+##modbusInterposeConfig("NDPSFlowMeter",0,5000,0)
 
 ## to 0   up 250 bytes(125 word), data block
-drvModbusAsynConfigure("NDPSAI",  "NDPSFlowMeter", 0, 3,  1002,   2, 7, 100, "Modicon")  
-drvModbusAsynConfigure("NDPSAI1",  "NDPSFlowMeter", 0, 3,  2002,   2, 7, 100, "Modicon")  
-drvModbusAsynConfigure("NDPSAI2",  "NDPSFlowMeter", 0, 3,  3002,   2, 7, 100, "Modicon")  
-drvModbusAsynConfigure("NDPSAI3",  "NDPSFlowMeter", 0, 3,  4002,   2, 7, 100, "Modicon")  
-drvModbusAsynConfigure("NDPSAI4",  "NDPSFlowMeter", 0, 3,  5002,   2, 7, 100, "Modicon")  
-drvModbusAsynConfigure("NDPSAI5",  "NDPSFlowMeter", 0, 3,  6002,   2, 7, 100, "Modicon")  
-drvModbusAsynConfigure("NDPSAI6",  "NDPSFlowMeter", 0, 3,  7002,   2, 7, 100, "Modicon")  
-drvModbusAsynConfigure("NDPSAI7",  "NDPSFlowMeter", 0, 3,  8002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI",  "NDPSFlowMeter", 0, 3,  1002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI1",  "NDPSFlowMeter", 0, 3,  2002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI2",  "NDPSFlowMeter", 0, 3,  3002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI3",  "NDPSFlowMeter", 0, 3,  4002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI4",  "NDPSFlowMeter", 0, 3,  5002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI5",  "NDPSFlowMeter", 0, 3,  6002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI6",  "NDPSFlowMeter", 0, 3,  7002,   2, 7, 100, "Modicon")  
+##drvModbusAsynConfigure("NDPSAI7",  "NDPSFlowMeter", 0, 3,  8002,   2, 7, 100, "Modicon")  
 
 ## Real Template
-dbLoadTemplate("db/flowMeter.sub")
+##dbLoadTemplate("db/flowMeter.sub")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
