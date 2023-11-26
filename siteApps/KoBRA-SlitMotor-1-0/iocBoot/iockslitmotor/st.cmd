bbfe5c0380824e9634b15ad7cc8b5430537321c8
Sangil-Lee <silee7103@gmail.com>
2023-11-26 11:14:10 +0900

    Modified DB

--- siteApps/KoBRA-SlitMotor-1-0/iocBoot/iockslitmotor/st.cmd
+++ siteApps/KoBRA-SlitMotor-1-0/iocBoot/iockslitmotor/st.cmd
@@ -18,10 +18,10 @@ kslitmotor_registerRecordDeviceDriver pdbbase
 drvAsynIPPortConfigure("SLITPLC", "192.168.0.2:502", 0, 0, 1)
 modbusInterposeConfig("SLITPLC",   0,5000,0)
 
-drvModbusAsynConfigure("KoBRAAI", "IFPLC", 0, 6,  0,   32,  7, 100, "Modicon")
-drvModbusAsynConfigure("KoBRAAO", "IFPLC", 0, 6,  128, 122, 7, 100, "Modicon")
-drvModbusAsynConfigure("KoBRADI", "IFPLC", 0, 3,  256, 4,   7, 100, "Modicon")
-drvModbusAsynConfigure("KoBRADO", "IFPLC", 0, 6,  260, 4,   7, 100, "Modicon")
+drvModbusAsynConfigure("KoBRAAI", "SLITPLC", 0, 6,  0,   32,  7, 100, "Modicon")
+drvModbusAsynConfigure("KoBRAAO", "SLITPLC", 0, 6,  128, 122, 7, 100, "Modicon")
+drvModbusAsynConfigure("KoBRADI", "SLITPLC", 0, 3,  256, 4,   7, 100, "Modicon")
+drvModbusAsynConfigure("KoBRADO", "SLITPLC", 0, 6,  260, 4,   7, 100, "Modicon")
 dbLoadTemplate("db/KoBRASlit.sub")
 
 cd "${TOP}/iocBoot/${IOC}"
