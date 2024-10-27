123e85a07daca7ca5802e3d5bdab1e9608087ffd
Sangil-Lee <silee7103@gmail.com>
2024-10-27 16:01:31 +0900

    IF autonics motor controller interface

--- siteApps/IF-Motor-Control-1-0/iocBoot/iocifmotor/st.cmd
+++ siteApps/IF-Motor-Control-1-0/iocBoot/iocifmotor/st.cmd
@@ -13,11 +13,13 @@ epicsEnvSet "STREAM_PROTOCOL_PATH" "../../proto"
 dbLoadDatabase "dbd/ifmotor.dbd"
 ifmotor_registerRecordDeviceDriver pdbbase
 
-##drvAsynIPPortConfigure ("IF-Motor-1", "192.168.:4001", 0, 0, 0)
+drvAsynIPPortConfigure ("IF-Motor-1", "192.168.:4001", 0, 0, 0)
 
 ## According to KoBRA Maganet Type
 ## Load record instances
 ##dbLoadTemplate("${TOP}/iocBoot/${IOC}/if-motor-1.sub", "DEVICE=IF-Motor:,   port=IF-Motor-1")
+#dbLoadTemplate("db/ifMotor.template", "SYS=IF, SUBSYS=-Target:, DEV=Sec1, SUBDEV=-Motor:, EEPROM_ADDR=E4, port=IF-Motor-1")
+dbLoadRecords("db/ifMotor.db", "SYS=IF, SUBSYS=-Target:, DEV=Sec1, SUBDEV=-Motor:, EEPROM_ADDR=E4, port=IF-Motor-1")
 
 
 cd "${TOP}/iocBoot/${IOC}"
