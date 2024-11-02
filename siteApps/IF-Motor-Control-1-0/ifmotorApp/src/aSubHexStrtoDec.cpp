0091fd181a2578c7e9772c5784f02d5579c5110b
Sangil-Lee <silee7103@gmail.com>
2024-11-02 16:05:03 +0900

    autonics motor controller

--- siteApps/IF-Motor-Control-1-0/ifmotorApp/src/aSubHexStrtoDec.cpp
+++ siteApps/IF-Motor-Control-1-0/ifmotorApp/src/aSubHexStrtoDec.cpp
@@ -17,12 +17,14 @@ static long ProcHexaStr2Dec(aSubRecord *precord)
 	long status = 0;
 
 	const char* hexstring = (const char*)precord->a;
+	//std::cout << hexstring << std::endl;
 	if(hexstring == nullptr) {
 	   return -1;
 	}
 
 	int number = (int)strtol(hexstring, NULL, 16);
-	precord->val = number;
+	long *uval = (long*)precord->vala;
+	uval[0] = number;
 
     return (status);
 }
