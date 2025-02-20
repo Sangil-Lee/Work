d41162f1c3076c051ae899816b5f1f3011b0e86e
Sangil-Lee <silee7103@gmail.com>
2025-02-21 00:25:18 +0900

    alarm setpoints changing

---
+++ siteApps/raonCryo_2025/alarmsetpoints/alarmsetpoints.py
@@ -0,0 +1,99 @@
+from epics import PV, caget, caput
+import argparse
+import csv
+
+def str2bool(s):
+    return s.lower() in ('yes', 'true', 't', '1')
+
+def setAlarms():
+    readfile = args.file
+    try:
+        with open(readfile, 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            for i, row in enumerate(csv_reader):
+                print(i, '===>>>')
+                pvname = ' '.join(row)
+
+                print('[Before]')
+                pv_hihi = PV(pvname+'.HIHI')
+                pv_high = PV(pvname+'.HIGH')
+                pv_low  = PV(pvname+'.LOW')
+                pv_lolo = PV(pvname+'.LOLO')
+
+                print(pvname+'.HIHI=', pv_hihi.get(), ' --> Set.HIHI = [', args.hihi, ']')
+                print(pvname+'.HIGH=', pv_high.get(), ' --> Set.HIGH = [', args.high, ']')
+                print(pvname+'.LOW=',  pv_low.get(),  ' --> Set.LOW  = [', args.low,  ']')
+                print(pvname+'.LOLO=', pv_lolo.get(), ' --> Set.LOLO = [', args.lolo, ']')
+
+                pv_hihi.put(float(args.hihi), wait=True)
+                pv_high.put(float(args.high), wait=True)
+                pv_low.put(float(args.low), wait=True)
+                pv_lolo.put(float(args.lolo), wait=True)
+
+                print('[After]')
+                print(pvname+'.HIHI=', pv_hihi.get())
+                print(pvname+'.HIGH=', pv_high.get())
+                print(pvname+'.LOW=',  pv_low.get())
+                print(pvname+'.LOLO=', pv_lolo.get())
+
+                print('<<<===')
+            file.close()
+    except IOError:
+        print('Exception:could not read file:', readfile)
+
+def checkAlarms():
+    readfile = args.file
+    try:
+        with open(args.file, 'r', encoding='utf-8') as file:
+            csv_reader = csv.reader(file)
+            for i, row in enumerate(csv_reader):
+                print(i, '===>>>')
+                pvname = ' '.join(row)
+
+                pv_hihi = PV(pvname+'.HIHI')
+                pv_high = PV(pvname+'.HIGH')
+                pv_low  = PV(pvname+'.LOW')
+                pv_lolo = PV(pvname+'.LOLO')
+
+                hihi_val = pv_hihi.get()
+                high_val = pv_high.get()
+                low_val  = pv_low.get()
+                lolo_val = pv_lolo.get()
+
+                if hihi_val != args.hihi or high_val != args.high or low_val  != args.low  or lolo_val != args.lolo :
+                    print(pvname + ".HIHI[{0}] --> Args.HIHI[{1}]".format(hihi_val, args.hihi) )
+                    print(pvname + ".HIGH[{0}] --> Args.HIGH[{1}]".format(high_val, args.high) )
+                    print(pvname + ".LOW[{0}]  --> Args.LOW[{1}]".format(low_val, args.low) )
+                    print(pvname + ".LOLO[{0}] --> Args.LOLO[{1}]".format(lolo_val, args.lolo) )
+                else:
+                    print("OK")
+
+                print('<<<===')
+            file.close()
+    except IOError:
+        print('Exception:could not read file:', readfile)
+
+parser = argparse.ArgumentParser(description='Alarm Setpoint Configure')
+
+parser.add_argument('-check', type=bool, default=argparse.SUPPRESS)
+parser.add_argument('-file',  help='Alarm list for changing alarm setpoints')
+parser.add_argument('-lolo',  type= float, help='LOLO alarm setpoint to change')
+parser.add_argument('-low',   type= float, help='LOW alarm setpoint to change')
+parser.add_argument('-high',  type= float, help='HIGH alarm setpoint to change')
+parser.add_argument('-hihi',  type= float, help='HIHI alarm setpoint to change')
+
+args = parser.parse_args()
+bcheck =str2bool('{}'.format('check' in args))
+
+print('Parsed arguments: {}'.format(args))
+print('-file', args.file)
+print('-lolo', args.lolo)
+print('-low',  args.low)
+print('-high', args.high)
+print('-hihi', args.hihi)
+
+if __name__ == "__main__":
+    if bcheck == True:
+        checkAlarms()
+    else:
+        setAlarms()
